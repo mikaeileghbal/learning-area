271e993edc23a524dc8c9c289fb0c2cdbbda6ed3
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-04-20 09:13:38 +0430

    add gitignore

--- javascript/MVC/LoginModel.js
+++
@@ -1,9 +0,0 @@
-export default LoginModel;
-
-const LoginModel = function (username, password, rememberme) {
-  this.UserName = username;
-  this.Password = password;
-  this.RemembrMe = rememberme;
-  this.LoginSuccessful = false;
-  this.LoginErrorMessage = "";
-};
