bfe62a2dcc94dda383919485c3c5f168884a58e5
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-04-27 09:26:16 +0430

    office reactjs upvote

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Comment from "./components/Comment";
+import ProductList from "./components/UpVote/ProductList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello react</h1>
+        <h1>React Components</h1>
       </header>
       <main className="app-main">
         <div className="component-container">
@@ -19,6 +20,12 @@ function App() {
             text="new commen"
             date="2022"
           />
+        </div>
+        <div className="component-container">
+          <h2 className="underline">Popular Products</h2>
+          <div className="content">
+            <ProductList />
+          </div>
         </div>
       </main>
     </div>
