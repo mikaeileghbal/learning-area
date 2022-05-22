d571659402d0e6257848f23073f211dc7f907156
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-05-22 09:21:47 +0430

    office pro-react todo bootstrap

--- pro-react/todo/src/App.js
+++ pro-react/todo/src/App.js
@@ -81,6 +81,17 @@ function App() {
           </table>
         )}
       </div>
+
+      <div className="m-2">
+        <div className="form-group">
+          <label>Name:</label>
+          <input className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>City:</label>
+          <input className="form-control" />
+        </div>
+      </div>
     </div>
   );
 }
