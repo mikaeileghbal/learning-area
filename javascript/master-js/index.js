c5bd23f650447b110d44e271d656d798cda461c2
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-06-01 11:09:16 +0430

    office javascript master-js functions

--- javascript/master-js/index.js
+++ javascript/master-js/index.js
@@ -24,3 +24,36 @@ console.log(NaN == NaN);
 
 let fortyTwo = 42;
 console.log(fortyTwo.toString()[1]);
+
+let x = null;
+if (!x && typeof x === "object") {
+  console.log("100% null");
+}
+
+console.log(typeof typeof 1);
+
+// function literal
+function add(a, b) {
+  return a + b;
+}
+
+// function expression
+const Add = function (a, b) {
+  return a + b;
+};
+
+// IIF
+(function sayHello() {
+  console.log("hello");
+})();
+
+// pass function to another function
+function changeCase(val) {
+  return val.toUpperCase();
+}
+
+function demFunction(a, passedFunction) {
+  console.log(passedFunction(a));
+}
+
+demFunction("smallcase", changeCase);
