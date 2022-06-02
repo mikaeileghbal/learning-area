aab1908770ee27467c9d0fcb2b89cee73a33dae7
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-06-02 09:52:45 +0430

    office pro-react sportsstore fix api problem

--- pro-react/sportsstore/src/data/RestDataSource.js
+++ pro-react/sportsstore/src/data/RestDataSource.js
@@ -2,7 +2,10 @@ import Axios from "axios";
 import { RestUrls } from "./Urls";
 
 export class RestDataSource {
-  GetData = (dataType) => this.SendRequest("get", RestUrls[dataType]);
+  GetData = (dataType) => {
+    console.log("inside RestDataSource");
+    return this.SendRequest("get", RestUrls[dataType]);
+  };
 
   SendRequest = (method, url) => Axios.request({ method, url });
 }
