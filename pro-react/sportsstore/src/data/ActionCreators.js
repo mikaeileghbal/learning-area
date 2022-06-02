ed9dbd6d4592d8c08b9fcf1aa2e54c022f06e138
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-06-02 10:44:31 +0430

    office pro-react sportsstore paginating

--- pro-react/sportsstore/src/data/ActionCreators.js
+++ pro-react/sportsstore/src/data/ActionCreators.js
@@ -4,16 +4,18 @@ import { RestDataSource } from "./RestDataSource";
 
 const dataSource = new RestDataSource();
 
-export const loadData = (dataType) => {
+export const loadData = (dataType, params) => {
   return {
     type: ActionTypes.DATA_LOAD,
     // payload: {
     //   dataType: dataType,
     //   data: initialData[dataType],
     // },
-    payload: dataSource.GetData(dataType).then((response) => ({
+    payload: dataSource.GetData(dataType, params).then((response) => ({
       dataType,
       data: response.data,
+      total: Number(response.headers["x-total-count"]),
+      params,
     })),
   };
 };
