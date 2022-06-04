80adc80abf5e901ec1a353c8d6e9cab5adffb217
Mikaeil <mikaeileghbal@yahoo.com>
2022-06-04 12:30:35 +0430

    home javascript master-js anonymous functions

--- javascript/master-js/index.js
+++ javascript/master-js/index.js
@@ -102,3 +102,86 @@ setActiveTab(function () {
 setActiveTab(function handler() {
   console.log("Setting active tab named function");
 }, 2);
+
+// Invocation as a function
+// If a function is not invoked as a method, constructor, or via apply() or call(),
+// it's simply invoked as a function
+// When a function is invoked with this pattern, this is bound to the global object.
+
+// for methods this is bound to the object on invocation
+const person = {
+  name: "Albert Einstein",
+  age: 66,
+  greet: function () {
+    console.log(this.name);
+  },
+};
+person.greet();
+
+// for constructor this is bound to newly created object
+
+const Person = function (name) {
+  this.name = name;
+};
+
+Person.prototype.greet = function () {
+  return this.name;
+};
+
+let albert = new Person("Albert Einstein");
+console.log(albert.greet());
+
+// Anonymous functions
+// Usage :
+
+// 1- while creating an object
+const santa = {
+  // anonymous function as method
+  say: function () {
+    console.log("santa ius here");
+  },
+};
+santa.say();
+
+// 2- while creating a list
+const things = [
+  // an array of anonymous functions
+  function () {
+    console.log("thing 1");
+  },
+  function () {
+    console.log("thing 2");
+  },
+];
+for (let i = 0; i < things.length; i++) {
+  things[i]();
+}
+
+// 3- anonymous functions as a parameter to another function
+function eventHandler(event) {
+  event();
+}
+eventHandler(function () {
+  console.log("event 1 fired");
+});
+eventHandler(function () {
+  console.log("event 2 fierd");
+});
+
+// 4- anonymous functions in conditional logic
+function draw(shape_name) {
+  let shape;
+  if (shape_name === "square") {
+    shape = function () {
+      console.log("Drawing square");
+    };
+  } else if (shape_name === "circle") {
+    shape = function () {
+      console.log("Drawing circle");
+    };
+  }
+  shape();
+}
+
+draw("square");
+draw("circle");
