2a3694ee08e83a8a8eba628ee4688342eae26025
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-06-13 09:26:33 +0430

    office pro-react lifecycle complete

--- pro-react/reactevents/src/lifecycle/LifeCycle.js
+++ pro-react/reactevents/src/lifecycle/LifeCycle.js
@@ -16,15 +16,18 @@ export default function LifeCycle() {
         <h1>LifeCycle</h1>
       </div>
       <div className="row p-2">
-        <div className="col-6">
+        <div className="col-4">
           <Message
             message={`Counter: ${counter}`}
             callback={incrementCounter}
             text="Increment Counter"
           />
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <List />
+        </div>
+        <div className="col-4">
+          <ExternalCunter />
         </div>
       </div>
     </div>
@@ -41,11 +44,27 @@ function ActionButton({ text, callback }) {
 }
 
 function Message({ text, callback, message }) {
+  const [showSpan, setShowSpan] = useState(false);
+
+  const handleClick = (e) => {
+    setShowSpan(!showSpan);
+    callback(e);
+  };
+
+  const getMessageElement = () => {
+    const div = (
+      <div id="messageDiv" className="h5 text-center p-2">
+        {message}
+      </div>
+    );
+    return showSpan ? <span>{div}</span> : div;
+  };
+
   console.log(`Render Message component`);
   return (
     <div>
-      <ActionButton theme="primary" text={text} callback={callback} />
-      <div className="h5 text-center p-2">{message}</div>
+      <ActionButton theme="primary" text={text} callback={handleClick} />
+      {getMessageElement()}
     </div>
   );
 }
@@ -62,8 +81,31 @@ function List() {
     <div>
       <ActionButton callback={reverseList} text="Reverse Names" />
       {names.map((name, index) => {
-        return <h5 key={name}>{name}</h5>;
+        return (
+          <h5 id={name.toLowerCase()} key={name}>
+            {name}
+          </h5>
+        );
       })}
+    </div>
+  );
+}
+
+let externalCounter = 0;
+
+function ExternalCunter() {
+  const [, updateState] = useState({});
+
+  const incrementCounter = () => {
+    externalCounter++;
+    console.log(externalCounter);
+    updateState({});
+  };
+
+  return (
+    <div>
+      <ActionButton callback={incrementCounter} text="External Counter" />
+      <div className="h5 text-center p-2">External: {externalCounter}</div>
     </div>
   );
 }
