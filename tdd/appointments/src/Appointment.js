71ec6437cb1d7e2e58b50ea5405bab3168f791b7
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-06-15 13:31:39 +0430

    office TDD lists

--- tdd/appointments/src/Appointment.js
+++ tdd/appointments/src/Appointment.js
@@ -1,17 +1,29 @@
 import React from "react";
 
+const appointmentTimeOfDay = (startsAt) => {
+  const [h, m] = new Date(startsAt).toTimeString().split(":");
+  return `${h}:${m}`;
+};
+
 export const Appointment = ({ customer }) => {
-  return <div>{customer.firstname}</div>;
+  return <div>{customer.firstName}</div>;
 };
 
 export const AppointmentsDayView = ({ appointments = [] }) => {
   return (
     <div id="appointmentsDayView">
       <ol>
-        {appointments.map((item) => (
-          <div />
+        {appointments.map((appointment) => (
+          <li key={appointment.startsAt}>
+            {appointmentTimeOfDay(appointment.startsAt)}
+          </li>
         ))}
       </ol>
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[0]} />
+      )}
     </div>
   );
 };
