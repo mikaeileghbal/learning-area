8db48b17f1f7aae23c3d0de73f7af668358c1f6b
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-06-16 12:36:03 +0430

    office tdd add webpack

---
+++ tdd/appointments/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
